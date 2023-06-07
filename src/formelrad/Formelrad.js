2b4a9fb2953ab35c134227326208333dd545308a
Giaciii <giacun.masueger@stud.kbw.ch>
2023-06-07 15:00:17 +0200

    Schleife hat gefehlt

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -72,7 +72,7 @@ export default function Formelrad() {
             setValues(values => ({...values, p: values.u * values.i}));
             setColors( colors=> ({...colors, r:"red", p:"red"}));
         }
-    }
+    }}
 
     return (
         <>
